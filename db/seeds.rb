03f6d957ac779bb8259f7920a3a377369a8be974
umakantrajpoot <rajput.umakant4@gmail.com>
2015-03-30 19:46:57 +0530

    add user signin signup feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: "admin@cp.com", password: 'crowd123', password_confirmation: 'crowd123', username: 'admin', first_name: 'admin', last_name: 'user', is_admin: true) if User.where(email: 'admin@cp.com').blank?
